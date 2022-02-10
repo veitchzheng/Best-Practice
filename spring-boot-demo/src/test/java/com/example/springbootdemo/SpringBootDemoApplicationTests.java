6d26443d28c4d591c86ed564d4f913626a1c6c70
zhengweichao <zhengweichao@guazi.com>
2022-02-10 14:36:49 +0800

    logback

--- spring-boot-demo/src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
+++ spring-boot-demo/src/test/java/com/example/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.springbootdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("unit test");
     }
 
 }
